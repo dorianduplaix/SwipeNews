4a6d78dbdb1f330474752095a8dfecff4806cbc9
odd <duplaix.dorian@yahoo.fr>
2024-03-19 10:41:49 +0100

    Initial Commit

---
+++ SwipeNews/SwipeNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeNewsApp.swift
+//  SwipeNews
+//
+//  Created by Dorian Duplaix on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwipeNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
